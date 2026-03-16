f2be03bcc2dfe7ad9c87cc4b7d2f6b165d0478d9
ermi-e3 <ermiyas_eshetu@outlook.com>
2026-03-16 16:01:21 +0300

    Create User Interface Design
    create WinForm User Interface

--- PostmanCloneUI/Dashboard.Designer.cs
+++ PostmanCloneUI/Dashboard.Designer.cs
@@ -28,21 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
+            formHeader = new Label();
+            apiLabel = new Label();
+            apiText = new TextBox();
+            callApi = new Button();
+            resultsText = new TextBox();
+            statusStrip = new StatusStrip();
+            systemStatus = new ToolStripStatusLabel();
+            resultsLabel = new Label();
+            statusStrip.SuspendLayout();
             SuspendLayout();
+            // 
+            // formHeader
+            // 
+            formHeader.AutoSize = true;
+            formHeader.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            formHeader.Location = new Point(64, 32);
+            formHeader.Name = "formHeader";
+            formHeader.Size = new Size(288, 54);
+            formHeader.TabIndex = 0;
+            formHeader.Text = "Postman Clone";
+            // 
+            // apiLabel
+            // 
+            apiLabel.AutoSize = true;
+            apiLabel.Location = new Point(64, 104);
+            apiLabel.Name = "apiLabel";
+            apiLabel.Size = new Size(69, 41);
+            apiLabel.TabIndex = 1;
+            apiLabel.Text = "API:";
+            // 
+            // apiText
+            // 
+            apiText.BorderStyle = BorderStyle.FixedSingle;
+            apiText.Location = new Point(126, 101);
+            apiText.Name = "apiText";
+            apiText.Size = new Size(557, 47);
+            apiText.TabIndex = 2;
+            // 
+            // callApi
+            // 
+            callApi.Location = new Point(689, 101);
+            callApi.Name = "callApi";
+            callApi.Padding = new Padding(0, 2, 0, 2);
+            callApi.Size = new Size(84, 47);
+            callApi.TabIndex = 3;
+            callApi.Text = "Go";
+            callApi.UseVisualStyleBackColor = true;
+            callApi.Click += callApi_Click;
+            // 
+            // resultsText
+            // 
+            resultsText.BackColor = Color.White;
+            resultsText.BorderStyle = BorderStyle.FixedSingle;
+            resultsText.Location = new Point(64, 219);
+            resultsText.Multiline = true;
+            resultsText.Name = "resultsText";
+            resultsText.ReadOnly = true;
+            resultsText.ScrollBars = ScrollBars.Both;
+            resultsText.Size = new Size(709, 319);
+            resultsText.TabIndex = 4;
+            // 
+            // statusStrip
+            // 
+            statusStrip.ImageScalingSize = new Size(20, 20);
+            statusStrip.BackColor = Color.White;
+            statusStrip.Items.AddRange(new ToolStripItem[] { systemStatus });
+            statusStrip.Location = new Point(0, 556);
+            statusStrip.Name = "statusStrip";
+            statusStrip.Size = new Size(808, 29);
+            statusStrip.TabIndex = 5;
+            statusStrip.Text = "System Status";
+            // 
+            // systemStatus
+            // 
+            systemStatus.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            systemStatus.Name = "systemStatus";
+            systemStatus.Size = new Size(56, 23);
+            systemStatus.Text = "Ready";
+            // 
+            // resultsLabel
+            // 
+            resultsLabel.AutoSize = true;
+            resultsLabel.Location = new Point(64, 175);
+            resultsLabel.Name = "resultsLabel";
+            resultsLabel.Size = new Size(111, 41);
+            resultsLabel.TabIndex = 6;
+            resultsLabel.Text = "Results";
             // 
             // Dashboard
             // 
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(808, 474);
+            BackColor = Color.White;
+            ClientSize = new Size(808, 585);
+            Controls.Add(resultsLabel);
+            Controls.Add(statusStrip);
+            Controls.Add(resultsText);
+            Controls.Add(callApi);
+            Controls.Add(apiText);
+            Controls.Add(apiLabel);
+            Controls.Add(formHeader);
             Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Margin = new Padding(6, 6, 6, 6);
+            Margin = new Padding(6);
             Name = "Dashboard";
             Text = "Postman Clone By Ermi";
             Load += Dashboard_Load;
+            statusStrip.ResumeLayout(false);
+            statusStrip.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label formHeader;
+        private Label apiLabel;
+        private TextBox apiText;
+        private Button callApi;
+        private TextBox resultsText;
+        private StatusStrip statusStrip;
+        private Label resultsLabel;
+        private ToolStripStatusLabel systemStatus;
     }
 }
--- PostmanCloneUI/Dashboard.cs
+++ PostmanCloneUI/Dashboard.cs
@@ -1,15 +1,37 @@
-namespace PostmanCloneUI
+namespace PostmanCloneUI;
+
+public partial class Dashboard : Form
 {
-    public partial class Dashboard : Form
+    public Dashboard()
     {
-        public Dashboard()
+        InitializeComponent();
+    }
+
+    private void Dashboard_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private async void callApi_Click(object sender, EventArgs e)
+    {
+        // Validate the API URL
+
+
+        try
         {
-            InitializeComponent();
+            systemStatus.Text = "Calling API....";
+
+            // Smple code - replace with the actual API call
+            await Task.Delay(2000); 
+
+            systemStatus.Text = "Ready";
+        }
+        catch (Exception ex)
+        {
+
+            resultsText.Text = "Error:" + ex.Message;
+            systemStatus.Text = "Error";
         }
 
-        private void Dashboard_Load(object sender, EventArgs e)
-        {
-
-        }
     }
 }
