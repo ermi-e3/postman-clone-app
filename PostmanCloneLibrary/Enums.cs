03b442a5816786ab0c8920f7097215663de47882
ermi-e3 <ermiyas_eshetu@outlook.com>
2026-04-24 21:35:39 +0300

    Api Get logic design

---
+++ PostmanCloneLibrary/Enums.cs
@@ -0,0 +1,6 @@
+﻿namespace PostmanCloneLibrary; 
+
+public enum HttpAction
+{
+    GET
+}
---
+++ PostmanCloneLibrary/IApiAccess.cs
@@ -0,0 +1,8 @@
+﻿
+namespace PostmanCloneLibrary;
+
+public interface IApiAccess
+{
+    Task<string> CallApiAsync(string url, bool formatOutPut = true, HttpAction action = HttpAction.GET);
+    bool IsValidUrl(string url);
+}
